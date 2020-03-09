bd37bd99761d5ea279311bcbc76834deaca19efb
Julian Feinauer <j.feinauer@pragmaticminds.de>
2020-03-09 11:28:56 +0100

    Current state. CDI not working (yet).

--- cdi-user/src/main/java/org/pragmaticminds/cdi/impl/UserService2.java
+++ cdi-user/src/main/java/org/pragmaticminds/cdi/impl/UserService2.java
@@ -1,23 +1,24 @@
 package org.pragmaticminds.cdi.impl;
 
-import org.osgi.service.component.annotations.Component;
 import org.pragmaticminds.karaf.DemoService;
 
-import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.Initialized;
+import javax.enterprise.event.Observes;
 import javax.inject.Inject;
+import javax.inject.Named;
 
-@Component(immediate = true)
+@Named
 public class UserService2 {
 
     @Inject
     DemoService demoService;
 
     public UserService2() {
-
+        System.out.println("Hello from CDI");
     }
 
-    @PostConstruct
-    void init() {
+    public void onStart(@Observes @Initialized(ApplicationScoped.class) Object init) {
         System.out.println("Hello from CDI: " + demoService.getMessage());
     }
 }
